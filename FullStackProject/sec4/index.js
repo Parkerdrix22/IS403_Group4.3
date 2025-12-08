c009916eff826ca46300be3e94cb187b316df0dd
isaacjohnson03 <ijohnso3@byu.edu>
2025-12-08 14:35:48 -0700

    fix knex

--- FullStackProject/sec4/index.js
+++ FullStackProject/sec4/index.js
@@ -7,6 +7,18 @@ const session = require("express-session");
 let path = require("path");
 let bodyParser = require("body-parser");
 
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME || "awseb-e-cmnauwhany-stack-awsebrdsdatabase-tj12s5abic9x.cb8eie2ew4fz.us-east-2.rds.amazonaws.com",
+        user: process.env.RDS_USERNAME || "project3",
+        password: process.env.RDS_PASSWORD || "project123",
+        database: process.env.RDS_DB_NAME || "ebdb",
+        port: process.env.RDS_PORT || 5432,
+        ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false 
+    }
+});
+
 let app = express();
 
 app.set("view engine", "ejs");
